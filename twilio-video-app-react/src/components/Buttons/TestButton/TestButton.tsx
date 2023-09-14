9a1be2b515695a42fbdcdd4f7de2f9d7e3f93c75
Hunter Shiells <h.shiells@hotmail.ca>
2023-09-14 10:21:22 -0400

    Implementation of /raisehand

--- twilio-video-app-react/src/components/Buttons/TestButton/TestButton.tsx
+++ twilio-video-app-react/src/components/Buttons/TestButton/TestButton.tsx
@@ -1,13 +1,20 @@
+import React from 'react';
 import Button from '@material-ui/core/Button';
 
 export default function TestButton() {
-  const openYouTube = () => {
-    window.open('https://www.youtube.com', '_blank');
+  const raiseHand = () => {
+    const newTab = window.open('https://98bd9ca92de136b9.p22.rt3.io/raisehand', '_blank');
+
+    if (newTab) {
+      window.setTimeout(() => {
+        newTab.close();
+      }, 2500);
+    }
   };
 
   return (
-    <Button onClick={openYouTube} variant="contained" color="primary">
-      Open YouTube
+    <Button onClick={raiseHand} variant="contained" color="primary">
+      Raise Hand
     </Button>
   );
 }
