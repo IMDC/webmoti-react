4289eba75f9dbf7fb61b8a567578bd9b81974843
Hunter Shiells <h.shiells@hotmail.ca>
2023-09-14 12:44:35 -0400

    Link fixing

--- twilio-video-app-react/src/components/Buttons/TestButton/TestButton.tsx
+++ twilio-video-app-react/src/components/Buttons/TestButton/TestButton.tsx
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 
 export default function TestButton() {
   const raiseHand = () => {
-    const newTab = window.open('https://4keaw6l3.connect.remote.it/raisehand', '_blank');
+    const newTab = window.open('https://y24khent.connect.remote.it/raisehand', '_blank');
 
     if (newTab) {
       window.setTimeout(() => {
