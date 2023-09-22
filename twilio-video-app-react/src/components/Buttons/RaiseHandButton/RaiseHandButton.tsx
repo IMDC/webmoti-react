dcde1c495c9af5f118a8d53acc4e50bdb1401ec4
Daniel Boxer <dani3lb0x3r@gmail.com>
2023-09-22 10:53:44 -0400

    added queue for raise hand

--- twilio-video-app-react/src/components/Buttons/RaiseHandButton/RaiseHandButton.tsx
+++ twilio-video-app-react/src/components/Buttons/RaiseHandButton/RaiseHandButton.tsx
@@ -1,4 +1,6 @@
 import Button from '@material-ui/core/Button';
+import { Message } from '@twilio/conversations';
+import { useEffect, useState } from 'react';
 import useChatContext from '../../../hooks/useChatContext/useChatContext';
 import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
 
@@ -10,7 +12,10 @@ export default function RaiseHandButton() {
 
   const { conversation } = useChatContext();
 
+  const [handQueue, setHandQueue] = useState<string[]>([]);
+
   const raiseHand = () => {
+    console.log(handQueue);
     conversation?.sendMessage(`${name} raised hand`);
 
     const newTab = window.open('https://y24khent.connect.remote.it/raisehand', '_blank');
@@ -21,6 +26,33 @@ export default function RaiseHandButton() {
       }, 12000);
     }
   };
+
+  // event listener for added msg
+  useEffect(() => {
+    const handleMessageAdded = (message: Message) => {
+      if (message.body) {
+        // check for raise hand msg format
+        const match = message.body.match(/^(.+) raised hand$/);
+
+        if (match) {
+          const name = match[1];
+          setHandQueue((prevQueue: string[]) => {
+            // add if not in queue already
+            if (!prevQueue.includes(name)) {
+              return [...prevQueue, name];
+            }
+            return prevQueue;
+          });
+        }
+      }
+    };
+
+    conversation?.on('messageAdded', handleMessageAdded);
+
+    return () => {
+      conversation?.off('messageAdded', handleMessageAdded);
+    };
+  }, [conversation]);
 
   return (
     <Button onClick={raiseHand} variant="contained" color="primary">
