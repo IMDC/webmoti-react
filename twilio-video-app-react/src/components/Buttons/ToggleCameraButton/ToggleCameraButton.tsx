6627142accf6edca84f230a0afa020d37f543406
Varshiga Kandeepan <varshigak@gmail.com>
2023-10-03 23:35:44 -0400

    Update ToggleCameraButton styling for consistency

--- twilio-video-app-react/src/components/Buttons/ToggleCameraButton/ToggleCameraButton.tsx
+++ twilio-video-app-react/src/components/Buttons/ToggleCameraButton/ToggleCameraButton.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import useWebmotiVideoContext from '../../../hooks/useWebmotiVideoContext/useWebmotiVideoContext';
 
-const ToggleCameraButton = () => {
+export default function ToggleCameraButton(props: { className?: string }) {
   const { toggleWebmotiVideo } = useWebmotiVideoContext();
 
-  const handleButtonClick = () => {
-    toggleWebmotiVideo();
-  };
-
   return (
-    <Button variant="contained" style={{ backgroundColor: 'transparent', color: 'black' }} onClick={handleButtonClick}>
+    <Button className={props.className} onClick={toggleWebmotiVideo}>
       Toggle Camera
     </Button>
   );
-};
-
-export default ToggleCameraButton;
+}
