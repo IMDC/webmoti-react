aeb992dc9c829db0f8524998c1d074f6b103338b
Daniel Boxer <dani3lb0x3r@gmail.com>
2023-12-07 16:39:07 -0500

    added get_url serverless function #15

---
+++ twilio-video-app-react/server/get_url.js
@@ -0,0 +1,63 @@
+/* global Twilio Runtime */
+"use strict";
+
+module.exports.handler = async function (context, event, callback) {
+  const client = context.getTwilioClient();
+  const response = new Twilio.Response();
+  response.setHeaders({ "Content-Type": "application/json" });
+
+  // from https://github.com/twilio-labs/plugin-rtc/blob/master/src/helpers.js
+
+  const APP_NAME = "video-app";
+
+  function getPasscode(domain, passcode) {
+    const [, appID, serverlessID] = domain.match(
+      /-?(\d*)-(\d+)(?:-\w+)?.twil.io$/
+    );
+    return `${passcode}${appID}${serverlessID}`;
+  }
+
+  async function findApp() {
+    const services = await client.serverless.services.list();
+    return services.find((service) => service.friendlyName.includes(APP_NAME));
+  }
+
+  async function getUrl() {
+    const app = await findApp();
+
+    if (!app) {
+      console.error("No deployed app found with the specified name.");
+      response.setStatusCode(404);
+      response.setBody({
+        error: "No deployed app found with the specified name.",
+      });
+      return callback(null, response);
+    }
+
+    const [environment] = await client.serverless
+      .services(app.sid)
+      .environments.list();
+    const variables = await client.serverless
+      .services(app.sid)
+      .environments(environment.sid)
+      .variables.list();
+
+    const passcodeVar = variables.find((v) => v.key === "API_PASSCODE");
+    const passcode = passcodeVar ? passcodeVar.value : "";
+
+    const fullPasscode = getPasscode(environment.domainName, passcode);
+
+    return `https://${environment.domainName}?passcode=${fullPasscode}`;
+  }
+
+  try {
+    const url = await getUrl();
+    response.setBody({ url: url });
+    return callback(null, response);
+  } catch (e) {
+    console.error(e);
+    response.setStatusCode(500);
+    response.setBody({ error: e.message });
+    return callback(null, response);
+  }
+};
