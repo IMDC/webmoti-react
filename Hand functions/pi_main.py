0d7c4471780d2ffcf5f30f9a5bf7233ee1ea1858
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-01-25 20:02:19 -0500

    formatted pi_main.py

--- Hand functions/pi_main.py
+++ Hand functions/pi_main.py
@@ -1,39 +1,43 @@
 import socket
 from time import sleep
+
 from RPi import GPIO
+
 
 # Function to load the modified HTML page
 def get_html():
-    with open('index.html', 'r') as file:
+    with open("index.html", "r") as file:
         html = file.read()
     return html
 
+
 # HTTP server with socket
-ethernet_ip = "141.117.145.158"  # Replace with your actual Ethernet IP
+ethernet_ip = "141.117.145.158"
 port = 80
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((ethernet_ip, port))
 s.listen(1)
 
-print('Listening on', (ethernet_ip, port))
+print("Listening on", (ethernet_ip, port))
 
-# Variables to store the hand status
-hand_status = "stopHand"  # Initialize to stopHand
 GPIO.setmode(GPIO.BOARD)
 servo_pin = 12
 GPIO.setup(servo_pin, GPIO.OUT)
 pwm = GPIO.PWM(servo_pin, 50)
 
+
 def set_servo_angle(angle):
-    duty_cycle = (angle / 18) + 2  # Convert angle to duty cycle (2 to 12)
+    # Convert angle to duty cycle (2 to 12)
+    duty_cycle = (angle / 18) + 2
     pwm.start(duty_cycle)
-    sleep(1)  # Adjust the sleep time as needed
+    sleep(1)
+
 
 # Listen for connections
 while True:
     conn, addr = s.accept()
-    print('Got a connection from %s' % str(addr))
+    print("Got a connection from %s" % str(addr))
 
     # Receive the request
     request = conn.recv(1024).decode()
@@ -42,26 +46,23 @@ while True:
     if "POST /raisehand" in request:
         # Run the "raiseHand" servo code
         try:
-            for _ in range(1):
-                set_servo_angle(160)
-                sleep(0.5)
-                set_servo_angle(0)
-                sleep(0.5)
-                set_servo_angle(160)
-                sleep(0.5)
-                set_servo_angle(0)
-                sleep(1)
-                # ... Add the rest of the servo code for "raiseHand" status
+            set_servo_angle(160)
+            sleep(0.5)
+            set_servo_angle(0)
+            sleep(0.5)
+            set_servo_angle(160)
+            sleep(0.5)
+            set_servo_angle(0)
+            sleep(1)
         except BrokenPipeError:
             print("Client disconnected before response completed.")
 
     # Load and serve the HTML page
     response = get_html()
     try:
-        conn.send(b'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        conn.send(response.encode('utf-8'))
+        conn.send(b"HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
+        conn.send(response.encode("utf-8"))
     except BrokenPipeError:
         print("Client disconnected before response completed.")
 
     conn.close()
-
