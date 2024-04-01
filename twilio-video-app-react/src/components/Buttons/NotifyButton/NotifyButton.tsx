4001bda013c36a143797b9ee581320acdb3ae56a
varshigak <varshigak@gmail.com>
2024-04-01 01:02:08 -0400

    Play sound out of profs speakers when notify button is clicked - closes #58

--- twilio-video-app-react/src/components/Buttons/NotifyButton/NotifyButton.tsx
+++ twilio-video-app-react/src/components/Buttons/NotifyButton/NotifyButton.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
+import useChatContext from '../../../hooks/useChatContext/useChatContext';
+import useWebmotiVideoContext from '../../../hooks/useWebmotiVideoContext/useWebmotiVideoContext';
 import soundsFile from '../../../sounds/ClearAnnounceTones.wav';
+import { JSONObject, Message } from '@twilio/conversations';
 
-export default function PlaySoundButton() {
-  const playSound = () => {
-    const audio = new Audio(soundsFile);
-    audio.play();
+export default function NotifyButton() {
+  const { conversation } = useChatContext();
+  const { sendSystemMsg, isProfessor } = useWebmotiVideoContext();
+
+  const notifyProfessor = () => {
+    if (!isProfessor) {
+      const audio = new Audio(soundsFile);
+      audio.play();
+      sendSystemMsg(conversation, 'Student needs attention');
+    }
   };
+
+  useEffect(() => {
+    const handleMessageAdded = (message: Message) => {
+      let isSystemMsg = false;
+      const attrObj = message.attributes as JSONObject;
+      if (attrObj.attributes !== undefined) {
+        const attrSysMsg = JSON.parse(attrObj.attributes as string).systemMsg;
+        if (attrSysMsg !== undefined) {
+          isSystemMsg = true;
+        }
+      }
+
+      if (isSystemMsg && message.body === 'Student needs attention' && isProfessor) {
+        const audio = new Audio(soundsFile);
+        audio.play();
+        message.remove();
+      }
+    };
+
+    conversation?.on('messageAdded', handleMessageAdded);
+
+    return () => {
+      conversation?.off('messageAdded', handleMessageAdded);
+    };
+  }, [conversation, isProfessor]);
 
   return (
     <Tooltip title="Click to grab professor's attention">
-      <Button onClick={() => playSound()}>Notify Professor</Button>
+      <Button onClick={notifyProfessor}>Notify Professor</Button>
     </Tooltip>
   );
 }
