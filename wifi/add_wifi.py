932d086f49e52fbac4b0354681b3f3781fb0506b
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-05-16 13:21:14 -0400

    added custom config option for add wifi

--- wifi/add_wifi.py
+++ wifi/add_wifi.py
@@ -11,7 +11,8 @@ USB_PATH = Path("D:\\")
 # SECURE: WPA-EAP + PEAP
 # REGULAR: WPA-PSK
 # OPEN: No password
-TYPES = ["SECURE", "REGULAR", "OPEN"]
+# CUSTOM: From file
+TYPES = ["SECURE", "REGULAR", "OPEN", "CUSTOM"]
 
 
 def md4_hash(input_str):
@@ -57,15 +58,135 @@ def get_open_config(ssid):
     """
 
 
+def validate_config(lines):
+    # empty list means any value is allowed
+    valid_keys = {
+        "ssid": {"type": str, "values": []},
+        "psk": {"type": str, "values": []},
+        "key_mgmt": {
+            "type": str,
+            "values": ["NONE", "WPA-PSK", "WPA-EAP", "IEEE8021X", "WPA-NONE"],
+        },
+        "proto": {"type": str, "values": ["WPA", "RSN", "WPA RSN"]},
+        "pairwise": {"type": str, "values": ["CCMP", "TKIP", "GTK_NOT_USED"]},
+        "group": {"type": str, "values": ["CCMP", "TKIP", "WEP40", "WEP104"]},
+        "priority": {"type": int, "values": []},
+        "scan_ssid": {"type": int, "values": [1, 0]},
+        "eap": {
+            "type": str,
+            "values": [
+                "PEAP",
+                "TLS",
+                "TTLS",
+                "PWD",
+                "SIM",
+                "AKA",
+                "AKA'",
+                "FAST",
+                "LEAP",
+            ],
+        },
+        "identity": {"type": str, "values": []},
+        "anonymous_identity": {"type": str, "values": []},
+        "password": {"type": str, "values": []},
+        "ca_cert": {"type": str, "values": []},
+        "client_cert": {"type": str, "values": []},
+        "private_key": {"type": str, "values": []},
+        "private_key_passwd": {"type": str, "values": []},
+        "phase1": {"type": str, "values": []},
+        "phase2": {"type": str, "values": []},
+        "bssid": {"type": str, "values": []},
+        "disabled": {"type": int, "values": [0, 1]},
+        "mode": {"type": int, "values": [0, 1, 2]},
+        "frequency": {"type": int, "values": []},
+        "proactive_key_caching": {"type": int, "values": [0, 1]},
+        "wep_key0": {"type": str, "values": []},
+        "wep_key1": {"type": str, "values": []},
+        "wep_key2": {"type": str, "values": []},
+        "wep_key3": {"type": str, "values": []},
+        "wep_tx_keyidx": {"type": int, "values": [0, 1, 2, 3]},
+        "auth_alg": {
+            "type": str,
+            "values": ["OPEN", "SHARED", "LEAP"],
+        },
+    }
+
+    config = ""
+
+    if lines[0].strip() != "network={":
+        stop("Invalid config start")
+    config += lines[0]
+    del lines[0]
+
+    if lines[-1].strip() != "}":
+        stop("Invalid config end")
+    del lines[-1]
+
+    for line in lines:
+        # only split on first =
+        try:
+            key, value = line.strip().split("=", maxsplit=1)
+        except Exception as e:
+            stop(f"Error while reading config line {line}: {e}")
+        key = key.strip()
+        # remove quotes around values
+        value = value.strip().strip('"')
+
+        if key not in valid_keys:
+            stop(f"Invalid key found: {key}")
+
+        expected_type = valid_keys[key]["type"]
+        try:
+            if expected_type == int:
+                value = int(value)
+            elif expected_type == str:
+                value = str(value)
+        except ValueError:
+            stop(
+                f"Wrong type for key {key}: expected {expected_type}, got {type(value)}"
+            )
+
+        # if value can't be anything and is invalid
+        if valid_keys[key]["values"] and value not in valid_keys[key]["values"]:
+            stop(f"Invalid value for key {key}: {value}")
+
+        # add line after validating
+        config += line
+
+    config += "}"
+    return config
+
+
+def get_custom_config(filename):
+    path = USB_PATH / filename
+
+    if not path.is_file():
+        stop("Wifi config file not found")
+
+    lines = []
+    try:
+        with path.open("r") as f:
+            lines = f.readlines()
+    except Exception as e:
+        stop(f"Error reading custom config file: {e}")
+
+    config = validate_config(lines)
+
+    return config
+
+
 def stop(msg):
     logging.error(f"{msg}\n")
     exit(1)
 
 
 def get_value(config, key):
-    value = config.get(CONFIG_SECTION, key, fallback=None).upper()
+    value = config.get(CONFIG_SECTION, key, fallback=None)
+    if value is None:
+        return None
+
     # allow for empty/none values in config file
-    if value in ("", "NONE"):
+    if value.upper() in ("", "NONE"):
         return None
     return value
 
@@ -77,7 +198,7 @@ def write_config(config_path):
 
     wifi_type = get_value(config, "type")
     logging.info(f"Wifi type: {wifi_type}")
-    if wifi_type not in TYPES:
+    if wifi_type is not None and wifi_type.upper() not in TYPES:
         stop("Invalid wifi type")
 
     wifi_ssid = get_value(config, "name")
@@ -104,8 +225,15 @@ def write_config(config_path):
             stop("Wifi password not found")
 
         config_str = get_psk_config(wifi_ssid, wifi_password)
-    else:
+    elif wifi_type == "OPEN":
         config_str = get_open_config(wifi_ssid)
+    else:
+        wifi_config_name = get_value(config, "filename")
+        logging.info(f"Wifi custom config: {wifi_config_name}")
+        if wifi_config_name is None:
+            stop("Custom config filename not found")
+
+        config_str = get_custom_config(wifi_config_name)
 
     wpa_conf_path = Path(WPA_PATH)
     if not wpa_conf_path.is_file():
@@ -118,8 +246,8 @@ def write_config(config_path):
 
 def main():
     if not USB_PATH.is_dir():
-        stop("USB not found")
-    logging.info("Found USB drive")
+        print("USB not found")
+        exit(1)
 
     # setup logging (logs are saved on usb key)
     log_format = "%(asctime)s - %(levelname)s - %(message)s"
@@ -134,6 +262,8 @@ def main():
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(logging.Formatter(log_format))
     logging.getLogger().addHandler(console_handler)
+
+    logging.info("Found USB drive")
 
     # make sure root access
     # if os.geteuid() != 0:
