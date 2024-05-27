d02504c17c14bbb1b84e93750bbc07f1b41a8957
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-05-27 15:35:18 -0400

    started multi hand #63

---
+++ multi-hand/reserve.js
@@ -0,0 +1,31 @@
+exports.handler = async function (context, event, callback) {
+  const syncServiceSid = context.TWILIO_SYNC_SERVICE_SID;
+  const syncMapName = context.SYNC_MAP_NAME;
+
+  try {
+    const syncClient = Runtime.getSync({ serviceName: syncServiceSid });
+    const syncMap = await syncClient.maps(syncMapName);
+    const hands = await syncMap.syncMapItems.list();
+
+    for (const hand of hands) {
+      const data = hand.data;
+      if (!data.isReserved) {
+        // reserve hand
+        await syncMap.syncMapItems(hand.key).update({
+          data: { ...data, isReserved: true },
+        });
+
+        // notify client
+        return callback(null, { handName: hand.key, urlId: data.urlId });
+      }
+    }
+  } catch (e) {
+    return callback("Error reserving hand");
+  }
+
+  const response = new Twilio.Response();
+  response.setStatusCode(404);
+  response.setBody("No hands available");
+
+  return callback(null, response);
+};
