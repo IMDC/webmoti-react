b5d1c5596f9d427ae8e41c16fa8dc488c7945b33
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-06-21 15:29:26 -0400

    queue display added hashed underline

--- Hand functions/app/static/queueListener.js
+++ Hand functions/app/static/queueListener.js
@@ -1,13 +1,33 @@
 const eventSource = new EventSource("/api/queue");
 
-eventSource.onmessage = function (event) {
+eventSource.onmessage = (event) => {
   const queue = JSON.parse(event.data);
 
   document.getElementById("counter").textContent = queue.length;
 
+  const headElement = document.getElementById("head");
+
   if (queue.length > 0) {
-    document.getElementById("head").textContent = queue[0];
+    const head = queue[0];
+    headElement.textContent = head;
+    headElement.style.textDecoration = `underline ${stringToColour(head)}`;
   } else {
-    document.getElementById("head").textContent = "";
+    headElement.textContent = "";
   }
+};
+
+const stringToColour = (str) => {
+  let hash = 0;
+  str.split("").forEach((char) => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash);
+  });
+  let colour = "#";
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xff;
+    // make all colours light for contrast
+    const minBrightness = 75;
+    value = Math.max(value, minBrightness);
+    colour += value.toString(16).padStart(2, "0");
+  }
+  return colour;
 };
