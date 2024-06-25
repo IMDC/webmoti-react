f1229f7aa9fcea7637174e5fe00e6741aab44fb2
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-06-25 14:41:57 -0400

    rasp pi check in utils

--- hand/app/utils.py
+++ hand/app/utils.py
@@ -1,13 +1,19 @@
 import logging
 import pathlib
+import platform
 import signal
 import sys
 from logging.handlers import RotatingFileHandler
 from time import sleep
 
-from RPi import GPIO
-
 from constants import LOG_PATH, SERVO_PIN
+
+# this is for testing
+is_rasp_pi = False
+if platform.system() == "Linux" and platform.machine() == "aarch64":
+    from RPi import GPIO
+
+    is_rasp_pi = True
 
 
 def setup_logging():
@@ -37,20 +43,23 @@ def setup_handlers():
 
 class ServoController:
     def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(SERVO_PIN, GPIO.OUT)
-        self.pwm = GPIO.PWM(SERVO_PIN, 50)
-        self.pwm.start(0)
+        if is_rasp_pi:
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(SERVO_PIN, GPIO.OUT)
+            self.pwm = GPIO.PWM(SERVO_PIN, 50)
+            self.pwm.start(0)
 
     def set_angle(self, angle):
-        # Convert angle to duty cycle (2 to 12)
-        duty_cycle = (angle / 18) + 2
-        self.pwm.ChangeDutyCycle(duty_cycle)
-        sleep(1.5)
+        if is_rasp_pi:
+            # Convert angle to duty cycle (2 to 12)
+            duty_cycle = (angle / 18) + 2
+            self.pwm.ChangeDutyCycle(duty_cycle)
+            sleep(1.5)
 
     def stop(self):
-        self.pwm.stop()
-        GPIO.cleanup()
+        if is_rasp_pi:
+            self.pwm.stop()
+            GPIO.cleanup()
 
 
 servo_controller = ServoController()
