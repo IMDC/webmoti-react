60d5461c5bdc9ddff76252232b19369bbfd5ff24
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-06-27 12:57:20 -0400

    wave hand buttons and loading

--- twilio-video-app-react/src/components/Buttons/WaveHandButton/WaveHandButton.tsx
+++ twilio-video-app-react/src/components/Buttons/WaveHandButton/WaveHandButton.tsx
@@ -1,4 +1,6 @@
-import { IconButton } from '@material-ui/core';
+import { useState } from 'react';
+
+import { Button, CircularProgress, Grid } from '@material-ui/core';
 import { EmojiPeople, PanTool } from '@material-ui/icons';
 import { useHotkeys } from 'react-hotkeys-hook';
 
@@ -8,12 +10,19 @@ import ShortcutTooltip from '../../ShortcutTooltip/ShortcutTooltip';
 export default function WaveHandButton() {
   const { sendHandRequest } = useWebmotiVideoContext();
 
+  const [isLoading1, setIsLoading1] = useState(false);
+  const [isLoading2, setIsLoading2] = useState(false);
+
   const wave = async () => {
+    setIsLoading1(true);
     await sendHandRequest('WAVE');
+    setIsLoading1(false);
   };
 
   const wave2 = async () => {
+    setIsLoading2(true);
     await sendHandRequest('WAVE2');
+    setIsLoading2(false);
   };
 
   useHotkeys('ctrl+3', (event) => {
@@ -27,18 +36,26 @@ export default function WaveHandButton() {
   });
 
   return (
-    <div>
-      <ShortcutTooltip shortcut="3" isCtrlDown>
-        <IconButton onClick={wave2}>
-          <PanTool />
-        </IconButton>
-      </ShortcutTooltip>
+    <Grid container spacing={2} justifyContent="center" alignItems="center">
+      <Grid item>
+        <ShortcutTooltip shortcut="3" isCtrlDown>
+          <Button onClick={wave} disabled={isLoading1} variant="contained">
+            Wave
+            <PanTool />
+            {isLoading1 && <CircularProgress size={24} />}
+          </Button>
+        </ShortcutTooltip>
+      </Grid>
 
-      <ShortcutTooltip shortcut="4" isCtrlDown>
-        <IconButton onClick={wave}>
-          <EmojiPeople />
-        </IconButton>
-      </ShortcutTooltip>
-    </div>
+      <Grid item>
+        <ShortcutTooltip shortcut="4" isCtrlDown>
+          <Button onClick={wave2} disabled={isLoading2} variant="contained">
+            Wave 2
+            <EmojiPeople />
+            {isLoading2 && <CircularProgress size={24} />}
+          </Button>
+        </ShortcutTooltip>
+      </Grid>
+    </Grid>
   );
 }
