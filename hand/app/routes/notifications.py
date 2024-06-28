b0e4370b5d505ebd3f17f2485d28a9488daa54fc
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-06-28 12:41:30 -0400

    confirmation notification

--- hand/app/routes/notifications.py
+++ hand/app/routes/notifications.py
@@ -17,6 +17,19 @@ vapid_email = os.getenv("VAPID_EMAIL")
 subscriptions: List[Dict] = []
 
 
+async def push(subscription, message):
+    try:
+        webpush(
+            subscription_info=subscription,
+            data=message,
+            vapid_private_key=vapid_private_key,
+            vapid_claims={"sub": vapid_email},
+        )
+        logging.info(f"Web push sent for subscription: {subscription}")
+    except WebPushException as e:
+        logging.error(f"Web push failed for subscription {subscription}: {e}")
+
+
 # this is async so it doesn't block
 async def send_notification(name):
     if not subscriptions:
@@ -24,21 +37,14 @@ async def send_notification(name):
         return
 
     for subscription in subscriptions:
-        try:
-            webpush(
-                subscription_info=subscription,
-                data=f"{name} has a question!",
-                vapid_private_key=vapid_private_key,
-                vapid_claims={"sub": vapid_email},
-            )
-            logging.info(f"Web push sent for subscription: {subscription}")
-        except WebPushException as e:
-            logging.error(f"Web push failed for subscription {subscription}: {e}")
+        push(subscription, f"{name} has a question!")
 
 
 @router.post("/save-subscription")
 async def raise_hand_endpoint(request: Request):
     subscription = await request.json()
     subscriptions.append(subscription)
+    # confirmation message
+    await push(subscription, "Notifications successfully enabled")
 
     return JSONResponse(content={"message": "Subscription saved"}, status_code=200)
