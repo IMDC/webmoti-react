7be3e9061313c82e11b8b1a817a9aae728ccee86
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-07-02 14:58:38 -0400

    updated move script for .env file

--- hand/move.sh
+++ hand/move.sh
@@ -5,12 +5,23 @@ DOWNLOAD_NAME="$DOWNLOAD_DIR/webmoti-W.I.P-main"
 
 if [ -f "$DOWNLOAD_NAME.zip" ]; then
     rm -rf "$DOWNLOAD_NAME"
+
+    # move .env temporarily
+    if [ -f "$HOME/app/.env" ]; then
+        mv "$HOME/app/.env" "$HOME/.env.backup"
+    fi
+
     rm -rf "$HOME/app"
 
     unzip "$DOWNLOAD_NAME.zip" -d "$DOWNLOAD_DIR"
 
     # copy app dir to home
     cp -r "$DOWNLOAD_NAME/hand/app" "$HOME"
+
+    # move .env file back
+    if [ -f "$HOME/.env.backup" ]; then
+        mv "$HOME/.env.backup" "$HOME/app/.env"
+    fi
 
     # remove zip
     rm -rf "$DOWNLOAD_NAME"
