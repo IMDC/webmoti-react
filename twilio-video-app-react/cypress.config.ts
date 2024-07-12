43d3227212039c1f87e13d1920704cd0d97f092b
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-07-12 09:52:17 -0400

    migrate to cypress 13

---
+++ twilio-video-app-react/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  video: false,
+  defaultCommandTimeout: 60000,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'cypress/reporter-config.json',
+  },
+  retries: 2,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+})
