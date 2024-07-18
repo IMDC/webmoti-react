bd474fe59d8bf225564318854efd11f45c685568
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-07-18 16:05:33 -0400

    added react-use-websocket

--- twilio-video-app-react/src/components/Buttons/ToggleCaptionsButton/ToggleCaptionsButton.tsx
+++ twilio-video-app-react/src/components/Buttons/ToggleCaptionsButton/ToggleCaptionsButton.tsx
@@ -1,5 +1,6 @@
 import { IconButton } from '@material-ui/core';
 import { ClosedCaption } from '@material-ui/icons';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { LocalAudioTrack } from 'twilio-video';
 
 import useIsTrackEnabled from '../../../hooks/useIsTrackEnabled/useIsTrackEnabled';
@@ -9,12 +10,21 @@ import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
 const isIOS = /iPhone|iPad/.test(navigator.userAgent);
 
 export default function ToggleCaptionsButton() {
-  const { localTracks } = useVideoContext();
+  const { localTracks, room } = useVideoContext();
 
   const localAudioTrack = localTracks.find((track) => track.kind === 'audio') as LocalAudioTrack;
 
   const isTrackEnabled = useIsTrackEnabled(localAudioTrack);
   const mediaStreamTrack = useMediaStreamTrack(localAudioTrack);
+
+  const identity = room?.localParticipant?.identity || 'Participant';
+
+  const WS_URL = 'ws://localhost:80/api/ws/stt';
+  const { sendMessage, readyState } = useWebSocket(WS_URL, {
+    queryParams: { identity },
+    share: true,
+    // shouldReconnect: (closeEvent) => true,
+  });
 
   const toggleCaptions = async () => {
     if (localAudioTrack && mediaStreamTrack && isTrackEnabled) {
@@ -23,20 +33,20 @@ export default function ToggleCaptionsButton() {
       const audioContext = new AudioContext();
       const source = audioContext.createMediaStreamSource(newMediaStream);
 
-      const ws = new WebSocket('ws://localhost:80/api/ws/stt');
+      // TODO make it detect silence
 
       const options = { mimeType: 'audio/webm; codecs=opus' };
       const recorder = new MediaRecorder(source.mediaStream, options);
 
       recorder.ondataavailable = (event) => {
-        if (ws.readyState === WebSocket.OPEN && event.data.size > 0) {
-          ws.send(event.data);
+        if (readyState === ReadyState.OPEN && event.data.size > 0) {
+          sendMessage(event.data);
         }
       };
 
       recorder.start(100);
 
-      ws.onclose = () => recorder.stop();
+      // ws.onclose = () => recorder.stop();
     }
   };
 
