b66a2ad0f46417a94cae0c657fd5dcb0236a880d
Daniel Boxer <dani3lb0x3r@gmail.com>
2024-10-10 13:54:06 -0400

    added openai assistant api for ai class notes

--- hand/app/routes/schedule.py
+++ hand/app/routes/schedule.py
@@ -5,7 +5,12 @@ from datetime import datetime
 from typing import Dict
 
 from fastapi import APIRouter, Depends, HTTPException, UploadFile
-from openai import OpenAI
+from openai import AsyncOpenAI
+from openai.types.beta.threads.message_create_params import (
+    Attachment,
+    AttachmentToolFileSearch,
+)
+from openai.types.beta.threads.run import Usage
 
 from core.models import ScheduleRequest
 
@@ -16,17 +21,18 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 # identity to schedule
 schedule: Dict[str, Dict[str, str]] = {}
 
-# client = OpenAI(api_key=openai_api_key)
+client = AsyncOpenAI(api_key=openai_api_key)
 
 
 def get_system_prompt():
     system_prompt = f"""
         Please analyze the class notes and times provided and generate a schedule in 
-        JSON format without any other text output. The schedule should have a number of 
-        topics based on the length of the notes. Set topic times based on the depth and 
-        complexity of the content, allocating more time to denser topics rather than 
-        distributing time evenly. Some topics may be hands on or demonstrations with 
-        few words and will need a longer time.
+        JSON format without any other text output. If no notes are provided, mostly empty, 
+        or inaccessible, return an error. Don't create a schedule from missing data. 
+        The schedule should have a number of topics based on the length of the notes. 
+        Set topic times based on the depth and complexity of the content, allocating more 
+        time to denser topics rather than distributing time evenly. Some topics may be hands 
+        on or demonstrations with few words and will need a longer time. 
 
         Output format:
         {{
@@ -34,92 +40,120 @@ def get_system_prompt():
             "<time 1 in 24h>": "<topic1>",
             "<time 2>": "<topic2>",
             "...": "...",
-            "...": "...",
-            "...": "..."
         }}
     """
     return textwrap.dedent(system_prompt)
 
 
-def get_schedule(
-    file: UploadFile, start_time: datetime, end_time: datetime
-) -> Dict[str, str]:
+async def get_assistant(client: AsyncOpenAI):
+    ASSISTANT_NAME = "Class Schedule Analyzer"
     system_prompt = get_system_prompt()
 
-    user_prompt = f"""
-        Start Time: {start_time}
-        End Time: {end_time}
-    """
+    a_list = await client.beta.assistants.list()
+    async for a in a_list:
+        # need to create new assistant if instructions change
+        if a.name == ASSISTANT_NAME and a.instructions == system_prompt:
+            print("found previous assistant")
+            return a
 
-    # completion = client.chat.completions.create(
-    #     model="gpt-4o-mini",
-    #     response_format={"type": "json_object"},
-    #     messages=[
-    #         {"role": "system", "content": system_prompt},
-    #         {"role": "user", "content": user_prompt},
-    #     ],
-    # )
-    # response = completion.choices[0].message.content
-
-    # make assistant and vector store
-    # assistant = client.beta.assistants.create(
-    #     name="Class Schedule Analyzer",
-    #     instructions=system_prompt,
-    #     model="gpt-4o-mini",
-    #     tools=[{"type": "file_search"}],
-    #     response_format={"type": "json_object"},
-    # )
-    # vector_store = client.beta.vector_stores.create(name="Class Notes")
-    # assistant = client.beta.assistants.update(
-    #     assistant_id=assistant.id,
-    #     tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
-    # )
-
-    # # make request with file and text
-    # notes_file = client.files.create(
-    #     file=open(file.filename, "rb"), purpose="assistants"
-    # )
-    # thread = client.beta.threads.create(
-    #     messages=[
-    #         {
-    #             "role": "user",
-    #             "content": user_prompt,
-    #             "attachments": [
-    #                 {"file_id": notes_file.id, "tools": [{"type": "file_search"}]}
-    #             ],
-    #         }
-    #     ]
-    # )
-
-    # # send request
-    # run = client.beta.threads.runs.create_and_poll(
-    #     thread_id=thread.id, assistant_id=assistant.id
-    # )
-    # messages = list(
-    #     client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id)
-    # )
-    # response = messages[0].content[0].text
-
-    # # delete vector store because files aren't needed now
-    # deleted_vector_store = client.beta.vector_stores.delete(
-    #     vector_store_id=vector_store.id
-    # )
-
-    example_response = json.dumps(
-        {
-            "title": "Eigenvalues and Eigenvectors",
-            "10:00": "Introduction to Eigenvalues and Eigenvectors",
-            "10:15": "Definition and Importance of Eigenvectors and Eigenvalues",
-            "10:30": "Example: Checking if Vectors are Eigenvectors",
-            "10:45": "Example: Eigenvalue and Eigenvector Computation",
-            "11:00": "General Procedure to Find Eigenvectors",
-            "11:15": "Example: Finding a Basis for Eigenspace",
-            "11:30": "Properties of Eigenvectors with Distinct Eigenvalues",
-            "11:45": "Implications of Zero Eigenvalues",
-        }
+    # assistant not found, create new one
+    print("creating new assistant")
+    return await client.beta.assistants.create(
+        name=ASSISTANT_NAME,
+        instructions=system_prompt,
+        model="gpt-4o-mini",
+        tools=[{"type": "file_search"}],
+        # json_object isn't available with file_search
+        # response_format={"type": "json_object"},
     )
 
-    response_data = json.loads(example_response)
+
+def calculate_token_cost(usage: Usage):
+    if not usage:
+        print("No usage information available")
+        return
+
+    prompt_tokens = usage.prompt_tokens
+    completion_tokens = usage.completion_tokens
+    total_tokens = usage.total_tokens
+
+    # gpt-4o mini
+    cost_per_1M_input_tokens = 0.150  # $0.150 per 1M input tokens
+    cost_per_1M_output_tokens = 0.600  # $0.600 per 1M output tokens
+
+    input_cost = (prompt_tokens / 1_000_000) * cost_per_1M_input_tokens
+    output_cost = (completion_tokens / 1_000_000) * cost_per_1M_output_tokens
+    total_cost = input_cost + output_cost
+
+    print(f"Prompt Tokens: {prompt_tokens}")
+    print(f"Completion Tokens: {completion_tokens}")
+    print(f"Total Tokens: {total_tokens}")
+    print(f"Input Cost (USD): ${input_cost:.4f}")
+    print(f"Output Cost (USD): ${output_cost:.4f}")
+    print(f"Total Cost (USD): ${total_cost:.4f}")
+
+
+async def query_assistant(file: UploadFile, query: str):
+    assistant = await get_assistant(client)
+
+    file_contents = await file.read()
+    file = await client.files.create(
+        file=(file.filename, file_contents, file.content_type), purpose="assistants"
+    )
+
+    thread = await client.beta.threads.create()
+    await client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        attachments=[
+            Attachment(
+                file_id=file.id, tools=[AttachmentToolFileSearch(type="file_search")]
+            )
+        ],
+        content=query,
+    )
+
+    run = await client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id, assistant_id=assistant.id
+    )
+
+    if run.status != "completed":
+        print("Run failed:", run.status)
+        raise HTTPException(status_code=500, detail="Error getting schedule")
+
+    message_list = await client.beta.threads.messages.list(
+        thread_id=thread.id, run_id=run.id
+    )
+    messages = [message async for message in message_list]
+
+    response = messages[0].content[0].text.value
+    print(response)
+
+    calculate_token_cost(run.usage)
+
+    # file isn't needed anymore
+    await client.beta.threads.delete(thread_id=thread.id)
+    await client.files.delete(file_id=file.id)
+
+    print("deleted thread and file")
+
+    return response
+
+
+async def get_schedule(
+    file: UploadFile, start_time: datetime, end_time: datetime
+) -> Dict[str, str]:
+    user_prompt = f"""
+        S: {start_time}
+        E: {end_time}
+    """
+
+    response = await query_assistant(file, user_prompt)
+
+    try:
+        response_data = json.loads(response)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=500, detail="Error getting schedule")
 
     return response_data
 
@@ -132,14 +166,15 @@ async def schedule_get_endpoint(identity: str) -> Dict[str, Dict[str, str]]:
     return {"schedule": schedule[identity]}
 
 
+# TODO add file limit and throttle
+# Also make it show all schedules uploaded by all users in frontend
+
+
 @router.post("/schedule")
 async def schedule_post_endpoint(
     file: UploadFile, form_data: ScheduleRequest = Depends()
 ) -> Dict[str, Dict[str, str]]:
-
-    # file_text = await file.read()
-
-    schedule[form_data.identity] = get_schedule(
+    schedule[form_data.identity] = await get_schedule(
         file, form_data.start_time, form_data.end_time
     )
 
