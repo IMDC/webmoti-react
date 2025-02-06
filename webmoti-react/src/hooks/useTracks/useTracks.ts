97d8556555170b01dda13231278aac234f3a8261
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-02-06 15:38:25 -0500

    temporary solution for excluding audio tracks

--- webmoti-react/src/hooks/useTracks/useTracks.ts
+++ webmoti-react/src/hooks/useTracks/useTracks.ts
@@ -1,19 +1,44 @@
-import { RemoteParticipant, RemoteTrack } from 'twilio-video';
 import { useEffect, useState } from 'react';
+
+import { RemoteParticipant, RemoteTrack } from 'twilio-video';
+
+import { WEBMOTI_CAMERA_1, WEBMOTI_CAMERA_2 } from '../../constants';
+import useVideoContext from '../useVideoContext/useVideoContext';
 
 export default function useTracks(participant: RemoteParticipant) {
   const [tracks, setTracks] = useState<RemoteTrack[]>([]);
+  const { room } = useVideoContext();
+
+  const name = room?.localParticipant?.identity || 'Participant';
 
   useEffect(() => {
     const subscribedTracks = Array.from(participant.tracks.values())
-      .filter(trackPublication => trackPublication.track !== null)
-      .map(trackPublication => trackPublication.track!);
+      .filter((trackPublication) => trackPublication.track !== null)
+      .map((trackPublication) => trackPublication.track!);
 
     setTracks(subscribedTracks);
 
-    const handleTrackSubscribed = (track: RemoteTrack) => setTracks(prevTracks => [...prevTracks, track]);
+    const handleTrackSubscribed = (track: RemoteTrack) => {
+      // TODO
+      // ! this is a temporary solution
+      // a better way would be to use server side subscription rules
+      // https://www.twilio.com/docs/video/api/track-subscriptions#static-example
+
+      // make the board-view unit ignore all tracks
+      if (name === WEBMOTI_CAMERA_2) {
+        return;
+      }
+
+      // make the student-view unit only render virtual student tracks and not the board-view track.
+      // this is to avoid the prof audio playing out of the speakers
+      if (name === WEBMOTI_CAMERA_1 && participant.identity === WEBMOTI_CAMERA_2) {
+        return;
+      }
+
+      setTracks((prevTracks) => [...prevTracks, track]);
+    };
     const handleTrackUnsubscribed = (track: RemoteTrack) =>
-      setTracks(prevTracks => prevTracks.filter(t => t !== track));
+      setTracks((prevTracks) => prevTracks.filter((t) => t !== track));
 
     participant.on('trackSubscribed', handleTrackSubscribed);
     participant.on('trackUnsubscribed', handleTrackUnsubscribed);
@@ -21,7 +46,7 @@ export default function useTracks(participant: RemoteParticipant) {
       participant.off('trackSubscribed', handleTrackSubscribed);
       participant.off('trackUnsubscribed', handleTrackUnsubscribed);
     };
-  }, [participant]);
+  }, [participant, name]);
 
   return tracks;
 }
