1bf38bc8ebb355f59357eb55513a7a5a2ba4dd5a
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-03-04 20:18:30 -0500

    updated servo_controller to use gpiozero library

--- hand/app/core/servo_controller.py
+++ hand/app/core/servo_controller.py
@@ -2,24 +2,21 @@ import asyncio
 import os
 import platform
 
+from gpiozero import AngularServo
+
 from core.constants import SERVO_PIN
 
 # this is for running on non rasp pi devices
 is_rasp_pi = False
 is_testing = os.getenv("PYTEST_RUNNING")
 if (platform.system() == "Linux" and platform.machine() == "aarch64") or is_testing:
-    from RPi import GPIO
-
     is_rasp_pi = True
 
 
 class ServoController:
     def __init__(self) -> None:
         if is_rasp_pi:
-            GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(SERVO_PIN, GPIO.OUT)
-            self.pwm = GPIO.PWM(SERVO_PIN, 50)
-            self.pwm.start(0)
+            self.servo = AngularServo(SERVO_PIN, min_angle=0, max_angle=180)
 
         # the lock is so multiple users can't use the servo at the same time
         self.lock = asyncio.Lock()
@@ -27,13 +24,9 @@ class ServoController:
 
     async def _set_angle(self, angle: float, sleep_time: float) -> None:
         if is_rasp_pi:
-            # convert angle to duty cycle (2 to 12)
-            duty_cycle = (angle / 18) + 2
-            self.pwm.ChangeDutyCycle(duty_cycle)
-            # wait a bit before stopping servo to remove momentum
+            self.servo.angle = angle
             await asyncio.sleep(sleep_time)
-            # relax servo to stop erratic movements
-            self.pwm.ChangeDutyCycle(0)
+            self.servo.angle = None
 
     async def set_angle(self, angle: float, sleep_time) -> None:
         async with self.lock:
@@ -48,8 +41,8 @@ class ServoController:
 
     def stop(self) -> None:
         if is_rasp_pi:
-            self.pwm.stop()
-            GPIO.cleanup()
+            self.servo.angle = None
+            self.servo.close()
 
 
 servo_controller = ServoController()
