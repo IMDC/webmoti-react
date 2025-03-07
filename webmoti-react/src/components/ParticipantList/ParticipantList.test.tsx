8be54d77cb1caff2954670bb737505f39c01cd02
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-03-07 11:52:46 -0500

    fixed tests

--- webmoti-react/src/components/ParticipantList/ParticipantList.test.tsx
+++ webmoti-react/src/components/ParticipantList/ParticipantList.test.tsx
@@ -1,12 +1,14 @@
 import EventEmitter from 'events';
-import React from 'react';
-import ParticipantList from './ParticipantList';
+
 import { shallow } from 'enzyme';
+
+import ParticipantList from './ParticipantList';
+import { WEBMOTI_CAMERA_1, WEBMOTI_CAMERA_2 } from '../../constants';
 import useMainParticipant from '../../hooks/useMainParticipant/useMainParticipant';
-import useScreenShareParticipant from '../../hooks/useScreenShareParticipant/useScreenShareParticipant';
-import useSelectedParticipant from '../VideoProvider/useSelectedParticipant/useSelectedParticipant';
 import useParticipantContext from '../../hooks/useParticipantsContext/useParticipantsContext';
+import useScreenShareParticipant from '../../hooks/useScreenShareParticipant/useScreenShareParticipant';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
+import useSelectedParticipant from '../VideoProvider/useSelectedParticipant/useSelectedParticipant';
 
 jest.mock('../../hooks/useParticipantsContext/useParticipantsContext');
 jest.mock('../../hooks/useVideoContext/useVideoContext');
@@ -27,14 +29,20 @@ describe('the ParticipantList component', () => {
     mockUseSelectedParticipant.mockImplementation(() => [null, () => {}]);
     mockRoom = new EventEmitter();
     mockRoom.localParticipant = 'localParticipant';
+    // default mock for mainParticipant to prevent TypeError
+    mockUseMainParticipant.mockImplementation(() => ({ sid: 'main', identity: 'other' }));
   });
 
   it('should correctly render Participant components', () => {
-    const mockParticipant = { sid: 2 };
+    const mockParticipant = { sid: '2', identity: WEBMOTI_CAMERA_1 };
     mockUseSelectedParticipant.mockImplementation(() => [mockParticipant, () => {}]);
     mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
     mockParticipantContext.mockImplementation(() => ({
-      speakerViewParticipants: [{ sid: 0 }, { sid: 1 }, mockParticipant],
+      speakerViewParticipants: [
+        { sid: '0', identity: WEBMOTI_CAMERA_1 },
+        { sid: '1', identity: WEBMOTI_CAMERA_2 },
+        mockParticipant,
+      ],
     }));
 
     const wrapper = shallow(<ParticipantList />);
@@ -42,23 +50,19 @@ describe('the ParticipantList component', () => {
   });
 
   it('should add the isSelected prop to the first remote participant when it is selected', () => {
-    const mockParticipant = { sid: 0 };
+    const mockParticipant = { sid: '0', identity: WEBMOTI_CAMERA_1 };
     mockUseSelectedParticipant.mockImplementation(() => [mockParticipant, () => {}]);
     mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
     mockParticipantContext.mockImplementation(() => ({
-      speakerViewParticipants: [mockParticipant, { sid: 1 }],
+      speakerViewParticipants: [mockParticipant, { sid: '1', identity: WEBMOTI_CAMERA_2 }],
     }));
 
     const wrapper = shallow(<ParticipantList />);
-    expect(
-      wrapper
-        .find('Memo(Participant)')
-        .at(1)
-        .prop('isSelected')
-    ).toBe(true);
+    // index 0 is the first remote participant
+    expect(wrapper.find('Memo(Participant)').at(0).prop('isSelected')).toBe(true);
   });
 
-  it('should not render anything when there are no remote particiants', () => {
+  it('should not render anything when there are no remote participants', () => {
     mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
     mockParticipantContext.mockImplementation(() => ({
       speakerViewParticipants: [],
@@ -69,62 +73,44 @@ describe('the ParticipantList component', () => {
   });
 
   it('should add the hideParticipant prop when the participant is the mainParticipant', () => {
-    const mockParticipant = { sid: 0 };
+    const mockParticipant = { sid: '0', identity: WEBMOTI_CAMERA_1 };
     mockUseMainParticipant.mockImplementation(() => mockParticipant);
     mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
     mockParticipantContext.mockImplementation(() => ({
-      speakerViewParticipants: [mockParticipant, { sid: 1 }],
+      speakerViewParticipants: [mockParticipant, { sid: '1', identity: WEBMOTI_CAMERA_2 }],
     }));
 
     const wrapper = shallow(<ParticipantList />);
-    expect(
-      wrapper
-        .find('Memo(Participant)')
-        .at(1)
-        .prop('hideParticipant')
-    ).toBe(true);
-
-    expect(
-      wrapper
-        .find('Memo(Participant)')
-        .at(2)
-        .prop('hideParticipant')
-    ).toBe(false);
+    // since mainParticipant.identity is WEBMOTI_CAMERA_1, allowedIdentitiesInFiltered is [WEBMOTI_CAMERA_2]
+    // index 0 (mainParticipant) should be hidden, index 1 shouldn't
+    expect(wrapper.find('Memo(Participant)').at(0).prop('hideParticipant')).toBe(true);
+    expect(wrapper.find('Memo(Participant)').at(1).prop('hideParticipant')).toBe(false);
   });
 
-  it('should not add the hideParticipant prop when the participant is the mainParticipant and they are selected', () => {
-    const mockParticipant = { sid: 0 };
+  it('should add the hideParticipant prop when the participant is the mainParticipant even if they are selected', () => {
+    const mockParticipant = { sid: '0', identity: WEBMOTI_CAMERA_1 };
     mockUseMainParticipant.mockImplementation(() => mockParticipant);
     mockUseSelectedParticipant.mockImplementation(() => [mockParticipant, () => {}]);
     mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
     mockParticipantContext.mockImplementation(() => ({
-      speakerViewParticipants: [mockParticipant, { sid: 1 }],
+      speakerViewParticipants: [mockParticipant, { sid: '1', identity: WEBMOTI_CAMERA_2 }],
     }));
 
     const wrapper = shallow(<ParticipantList />);
-    expect(
-      wrapper
-        .find('Memo(Participant)')
-        .at(1)
-        .prop('hideParticipant')
-    ).toBe(false);
+    // when showAll is false, hideParticipant is based on identity, not selection
+    expect(wrapper.find('Memo(Participant)').at(0).prop('hideParticipant')).toBe(true);
   });
 
-  it('should not add the hideParticipant prop when the participant is the mainParticipant and they are sharing their screen', () => {
-    const mockParticipant = { sid: 0 };
+  it('should add the hideParticipant prop when the participant is the mainParticipant even if they are sharing their screen', () => {
+    const mockParticipant = { sid: '0', identity: WEBMOTI_CAMERA_1 };
     mockUseMainParticipant.mockImplementation(() => mockParticipant);
     mockUseScreenShareParticipant.mockImplementation(() => mockParticipant);
     mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
     mockParticipantContext.mockImplementation(() => ({
-      speakerViewParticipants: [mockParticipant, { sid: 1 }],
+      speakerViewParticipants: [mockParticipant, { sid: '1', identity: WEBMOTI_CAMERA_2 }],
     }));
 
     const wrapper = shallow(<ParticipantList />);
-    expect(
-      wrapper
-        .find('Memo(Participant)')
-        .at(1)
-        .prop('hideParticipant')
-    ).toBe(false);
+    expect(wrapper.find('Memo(Participant)').at(0).prop('hideParticipant')).toBe(true);
   });
 });
