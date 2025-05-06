f6271cf04aca9774e88cdff48b8ff29632d48b57
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-05-06 15:05:27 -0400

    setup react testing library

--- webmoti-react/jest.config.js
+++ webmoti-react/jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFiles: ['<rootDir>/src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   reporters: ['default', 'jest-junit'],
 
   // We don't need to test the static JSX in the icons folder, so let's exclude it from our test coverage report
--- webmoti-react/src/setupTests.ts
+++ webmoti-react/src/setupTests.ts
@@ -1,6 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'isomorphic-fetch';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 
 configure({ adapter: new Adapter() });
 
