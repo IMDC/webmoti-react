b2a8bf6f8fd670cd94f3f346697eaa824529017b
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-05-15 12:40:55 -0400

    removed react-test-renderer package

--- webmoti-react/src/components/NetworkQualityLevel/NetworkQualityLevel.test.tsx
+++ webmoti-react/src/components/NetworkQualityLevel/NetworkQualityLevel.test.tsx
@@ -1,22 +1,22 @@
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+
 import NetworkQualityLevel from './NetworkQualityLevel';
 
 describe('the NetworkQualityLevel component', () => {
+  const mockParticipant = (level: number) => ({ networkQualityLevel: level, on: () => {} }) as any;
+
   it('should render correctly for level 5', () => {
-    const mockParticipant = { networkQualityLevel: 5, on: () => {} } as any;
-    const tree = renderer.create(<NetworkQualityLevel participant={mockParticipant} />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container } = render(<NetworkQualityLevel participant={mockParticipant(5)} />);
+    expect(container).toMatchSnapshot();
   });
 
   it('should render correctly for level 3', () => {
-    const mockParticipant = { networkQualityLevel: 3, on: () => {} } as any;
-    const tree = renderer.create(<NetworkQualityLevel participant={mockParticipant} />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container } = render(<NetworkQualityLevel participant={mockParticipant(3)} />);
+    expect(container).toMatchSnapshot();
   });
 
   it('should render correctly for level 0', () => {
-    const mockParticipant = { networkQualityLevel: 0, on: () => {} } as any;
-    const tree = renderer.create(<NetworkQualityLevel participant={mockParticipant} />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container } = render(<NetworkQualityLevel participant={mockParticipant(0)} />);
+    expect(container).toMatchSnapshot();
   });
 });
