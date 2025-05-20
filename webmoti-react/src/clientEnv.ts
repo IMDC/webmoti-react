032e46f5b5f0da688e8649b86416a3df0167b109
DanielBoxer <65575771+DanielBoxer@users.noreply.github.com>
2025-05-20 13:27:57 -0400

    added clientEnv and updated setup.md for new env

---
+++ webmoti-react/src/clientEnv.ts
@@ -0,0 +1,14 @@
+export const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+export const FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+export const FIREBASE_STORAGE_BUCKET = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+export const FIREBASE_MESSAGING_SENDER_ID = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+
+export const API_DOMAIN = import.meta.env.VITE_API_DOMAIN;
+
+export const LIVEKIT_URL = import.meta.env.VITE_LIVEKIT_URL;
+
+export const SET_AUTH = import.meta.env.VITE_SET_AUTH;
+
+export const DISABLE_TWILIO_CONVERSATIONS = import.meta.env.VITE_DISABLE_TWILIO_CONVERSATIONS;
+
+export const TOKEN_ENDPOINT = import.meta.env.VITE_TOKEN_ENDPOINT;
